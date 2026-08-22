d7964d62a7d6c8d8a938d44fdc4dc8359125cfc2
masterhafizibrahim-sys <masterhafizibrahim@gmail.com>
2026-08-22 18:19:49 +1000

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,4 +2,4 @@ import os
 
 url = "https://www.youtube.com/@HisYTStory/videos"
 
-os.system(f'yt-dlp -N 32 --concurrent-fragments 8 --force-ipv4 -f "bv*[height<=720]+ba/b" {URL}')
+os.system(f'yt-dlp -N 32 --concurrent-fragments 8 --force-ipv4 -f "bv*[height<=720]+ba/b" {url}')
