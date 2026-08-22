ed52746c1085c3f85d214cdcd9d6796f8a4a734a
masterhafizibrahim-sys <masterhafizibrahim@gmail.com>
2026-08-22 18:18:14 +1000

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,5 +1,5 @@
 import os
 
-URL = "https://www.youtube.com/playlist?list=UULFHisYTStory"
+url = "https://www.youtube.com/@HisYTStory/videos"
 
 os.system(f'yt-dlp -N 32 --concurrent-fragments 8 --force-ipv4 -f "bv*[height<=720]+ba/b" {URL}')
