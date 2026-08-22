87a860d40735dac5721026f80a30723a10040ea7
masterhafizibrahim-sys <masterhafizibrahim@gmail.com>
2026-08-22 23:23:10 +1000

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,4 +2,4 @@ import os
 
 url = "https://www.youtube.com/@HisYTStory/videos"
 
-os.system(f'yt-dlp --cookies cookies.txt -f "bv*[height<=720]+ba/b" --force-ipv4 {url}')
+os.system(f'yt-dlp --cookies cookies.txt --extractor-args "youtube:player_client=web" -f "bv*[height<=720]+ba/b" --force-ipv4 {url}')
